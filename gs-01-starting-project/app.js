66823d79f8b8a3b643cd1841c4bf84aaed827d44
Kinyua Mwaniki <kinyuafelix4@gmail.com>
2020-12-24 12:15:30 +0300

    Lesson 6

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,14 +1,29 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue : ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
 
-function addGoal()
-{
-    const enteredValue = inputEl.value;
-    const listItemElement = document.createElement('li');
-    listItemElement.textContent = enteredValue;
-    listEl.appendChild(listItemElement);
-    inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addGoal);
+// function addGoal()
+// {
+//     const enteredValue = inputEl.value;
+//     const listItemElement = document.createElement('li');
+//     listItemElement.textContent = enteredValue;
+//     listEl.appendChild(listItemElement);
+//     inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
+
