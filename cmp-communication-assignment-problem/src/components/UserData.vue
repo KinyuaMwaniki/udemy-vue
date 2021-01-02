79884d0a166cbb9bcd604092a7190ed8ef59613f
Felix Mwaniki <kinyuafelix4@gmail.com>
2021-01-02 10:26:19 +0300

    Finish git cmp communication assignment

---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <form @submit.prevent="addUser">
+      <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input
+          type="text"
+          class="form-control"
+          id="text"
+          aria-describedby="name"
+          v-model="enteredName"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="age" class="form-label">Age</label>
+        <input
+          type="number"
+          class="form-control"
+          id="number"
+          v-model="enteredAge"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  emits: ["add-user"],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: 0,
+    };
+  },
+  methods: {
+    addUser() {
+      this.$emit("add-user", this.enteredName, this.enteredAge);
+    },
+  },
+};
+</script>
