603fff812d9653f50e4ab8ef52b8b29beab551bd
Felix Mwaniki <kinyuafelix4@gmail.com>
2021-01-07 23:30:59 +0300

    Finish 133

--- prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -1,4 +1,5 @@
 <template>
+
   <ul>
     <resource-item
       v-for="res in resources"
@@ -16,7 +17,7 @@ export default {
   components: {
     ResourceItem,
   },
-  props: ['resources'],
+  inject: ['resources'],
 };
 </script>
 
