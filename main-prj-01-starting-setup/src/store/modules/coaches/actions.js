178e3272333a4501a2b2a08279bf1567e2a40c7c
Felix Mwaniki <kinyuafelix4@gmail.com>
2021-03-02 11:20:25 +0300

    Finish main project

---
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -0,0 +1,60 @@
+export default {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.userId;
+    const coachData = {
+      firstName: payload.first,
+      lastName: payload.last,
+      description: payload.desc,
+      hourlyRate: payload.rate,
+      areas: payload.areas
+    };
+
+    const response = await fetch(
+      `https://udemymainproject-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      }
+    );
+
+    if (!response.ok) {
+      // error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
+  },
+
+  async loadCoaches(context, payload) {
+
+    if(!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+    const response = await fetch(
+      `https://udemymainproject-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to Fetch');
+      throw error;
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coachData = {
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+        id: key,
+      };
+      coaches.push(coachData);
+    }
+
+    context.commit('setCoaches', coaches)
+    context.commit('setFetchTimeStamp'); 
+  }
+};
